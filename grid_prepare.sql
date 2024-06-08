cfa7b019d96ba94472e46f6ad2d46c3ccf2fdaa9
Peter <peter@addressforall.org>
2024-06-08 11:51:36 -0300

    temporary

--- grid_prepare.sql
+++ grid_prepare.sql
@@ -11,50 +11,16 @@ CREATE TABLE grid_cm.all_levels(
  UNIQUE(code_b16h),
  UNIQUE(geom)
 );
-------
-DROP FUNCTION IF EXISTS grid_cm.generate_all_levels;
-;
-CREATE FUNCTION grid_cm.generate_all_levels(
-  p_lev_max numeric,
-  p_null_on_geom4326 boolean DEFAULT false
-) RETURNS TABLE ( -- deu grid_cm.all_levels
- gid_vbit  varbit,
- hlevel    real, 
- code_b16h text  ,
- geom      geometry,
- geom4326  geometry
-) AS $f$
-DECLARE
-  tpl text;
-  s text;
-  lev numeric;
-  gg text :='geom4326';
-BEGIN
- tpl := $$
-   SELECT cbits as gid_vbit, %1$s::real as hlevel, code as code_b16h, geom, geom4326
-   FROM osmc.decode_scientific_absolute_geoms(
-     osmc.cover_scientific_codes(%1$s, '{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e}')::text, 'CM', 16
-   ) t
- $$;
- IF p_null_on_geom4326 THEN gg := 'NULL'; END IF;
- s := format(tpl,'0');
- FOR lev IN (select x from generate_series(0.5, p_lev_max, 0.5) t(x)) LOOP
-    s := s || E'\n UNION ALL \n'|| format(tpl,lev::text);
-  END LOOP;
-  s := s|| E'\n   ORDER BY 1';
-  RETURN QUERY EXECUTE s;
-END;
-$f$ LANGUAGE PLpgSQL IMMUTABLE;
--- comment??
-
 -----
+function grid_cm.generate_all_levels() returns TABLE LIKE grid_cm.all_levels ?? see osmc.grid_generate_all_levels(4.0, 'CM', '{1,2,3,4,5,6,7,8,9,a,b,c,d,e}', 16, 0.005, 0.00000005);
+------
 INSERT INTO grid_cm.all_levels
   SELECT * FROM grid_cm.generate_all_levels(4.0)
 ;
 INSERT INTO grid_cm.all_levels
-  SELECT * FROM grid_cm.generate_all_levels(5.5,true)
+  SELECT * FROM grid_cm.generate_all_levels(5.5,true) -- Melhorar!
   WHERE hlevel>4.0
-;
+; -- no update?
 CREATE VIEW grid_cm.vw_level00_0 AS SELECT * FROM grid_cm.all_levels WHERE hlevel=0;
 CREATE VIEW grid_cm.vw_level00_5 AS SELECT * FROM grid_cm.all_levels WHERE hlevel=0.5;
 CREATE VIEW grid_cm.vw_level01_0 AS SELECT * FROM grid_cm.all_levels WHERE hlevel=1.0;
